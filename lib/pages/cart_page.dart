5613eecd505641427e3b93d2476c5a650d625aff
FernandoRCandiani <candianirfernando@gmail.com>
2022-11-28 12:04:49 -0300

    Listing products in cart

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/cart_item.dart';
+import 'package:shop/models/cart.dart';
+import 'package:shop/models/cart_item.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({
@@ -7,9 +11,60 @@ class CartPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Cart cart = Provider.of(context);
+    final items = cart.items.values.toList();
+
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text('Carrinho'),
+      ),
+      body: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.all(25),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Total',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(width: 10),
+                  Chip(
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    label: Text(
+                      'R\$ ${cart.totalAmount}',
+                      style: TextStyle(
+                        color:
+                            Theme.of(context).primaryTextTheme.headline6?.color,
+                      ),
+                    ),
+                  ),
+                  Spacer(),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text('COMPRAR'),
+                    style: TextButton.styleFrom(
+                        textStyle: TextStyle(
+                      color: Theme.of(context).colorScheme.primary,
+                    )),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (ctx, i) => CartItemWidget(cartItem: items[i]),
+            ),
+          ),
+        ],
       ),
     );
   }
